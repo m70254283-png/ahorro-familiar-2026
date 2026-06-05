5b573c91124d8c6e1528367e3269cf71354c448f
m70254283-png <m70254283@gmail.com>
2026-06-05 12:30:07 -0400

    Add files via upload
    corregiendo el firebase

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebase
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrg-aB6l0Y4m4mJuQnDsvTFgzQOttc7M",
+    apiKey: "AIzaSyCrg-aB6l0Y4m4mJuQnDsvTFgzQOttc7MA",
     authDomain: "ahorro-familiar-f6050.firebaseapp.com",
     databaseURL: "https://ahorro-familiar-f6050-default-rtdb.firebaseio.com",
     projectId: "ahorro-familiar-f6050",
